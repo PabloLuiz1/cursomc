7f8eee54cdb2b9a8904f56ffef60f403f3c8678e
Pablo Luiz <pabloluiz.ribeiroo@gmail.com>
2019-10-09 17:11:52 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/pabloluiz/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.pabloluiz.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pabloluiz.cursomc.domain.Categoria;
+import com.pabloluiz.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
