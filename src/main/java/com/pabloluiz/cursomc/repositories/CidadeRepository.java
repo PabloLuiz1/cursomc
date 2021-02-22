02980bd18832d1166cd7b02712cf3ac95681bd7c
PabloLuiz1 <pabloluiz.ribeiroo@gmail.com>
2021-02-22 01:41:35 -0300

    Estado e cidade

---
+++ src/main/java/com/pabloluiz/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.pabloluiz.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pabloluiz.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
